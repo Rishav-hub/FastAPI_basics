3bdf4d06d186143333cf87ce19ca1e8946d3cdc8
Rishav-hub <9930046@gmail.com>
2022-03-01 14:36:42 +0530

    error resolved with postman

--- TodoList/models.py
+++ TodoList/models.py
@@ -1,28 +1,31 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
 
+
 class Users(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
+
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String(120), unique=True, index=True)
-    username = Column(String(50), unique=True, index=True)
-    first_name = Column(String(50))
-    last_name = Column(String(50))
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
 
-    todos = relationship('Todos', back_populates='owner')
+    todos = relationship("Todos", back_populates="owner")
 
 
 class Todos(Base):
-    __tablename__ = 'todos'
+    __tablename__ = "todos"
+
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(100))
-    description = Column(String(1000))
+    title = Column(String)
+    description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
-    owner_id = Column(Integer, ForeignKey('users.id'))
+    owner_id = Column(Integer, ForeignKey("users.id"))
 
-    owner = relationship('Users', back_populates='todos')
+    owner = relationship("Users", back_populates="todos")
     
